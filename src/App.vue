05e9309658af3f80a7f86a7b43b8a427b06cdbe9
Afzalbek <uzafo.dev@gmail.com>
2022-01-18 17:10:00 +0500

    The site was built successfully

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="navigation">
+        <header>
+          <h1>Afzalbek Orifjonov</h1>
+        </header>
+        <nav>
+          <a href="#">ABOUT US</a>
+          <a href="#">CONTACT</a>
+          <a href="#">PORTFOLIO</a>
+        </nav>
+      </div>
+      <Progressbar :progress="progress" />
+      <Post @progressUpdate="setProgress" />
+    </main>
+  </div>
+</template>
+
+<script>
+import Post from './components/Post'
+import Progressbar from './components/Progressbar'
+export default {
+  name: 'App',
+  components: {
+    Post,
+    Progressbar
+  },
+  data(){
+    return {
+      progress: 0,
+    }
+  },
+  methods: {
+    setProgress(progress) {
+      this.progress = progress;
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'monospace', sans-serif;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+.navigation {
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #002337;
+  padding: 30px;
+}
+
+header h1 {
+  color: #fff;
+  font-size: 32px;
+}
+
+nav a {
+  color: #fff;
+  text-decoration: none;
+  padding: 7px;
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,65 @@
+<template>
+    <article>
+        <div class="content">
+            <h2>Euro 2022</h2>
+            <h3>Final (Italy and England)</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, perferendis. Aliquid amet numquam et nemo! Rem excepturi, blanditiis eius non quasi, molestias facilis ea, temporibus nesciunt consequuntur expedita laboriosam. Pariatur dolore fugiat accusantium quod corrupti officiis laudantium inventore aliquid nihil laboriosam tempore vel officia iusto quos doloribus ipsum neque modi, ex corporis, molestias error eligendi animi ratione! Doloremque nulla iste culpa eos, adipisci aut sapiente, ex animi, ab ratione minima nemo quas a perspiciatis. Quae nostrum placeat id culpa, enim illum, sed vero saepe harum dolorum alias quos! Facere adipisci nostrum est exercitationem suscipit corrupti incidunt architecto quo animi sed.</p>
+            <p>Animi, repellat. Quidem delectus, laborum beatae sapiente alias ullam aspernatur eaque repellat eos accusamus voluptas. Debitis, voluptas laborum ipsa cupiditate harum veritatis aspernatur necessitatibus quod ad a, quis voluptatum assumenda libero aut eveniet. Maiores reiciendis quis quisquam, iste veritatis in. Totam maiores eveniet libero omnis earum sapiente esse fuga aliquid minus, inventore voluptas quo adipisci id sint error, aut odit natus. Voluptatibus fugiat hic incidunt perspiciatis molestiae nihil, et, provident maiores consequuntur quidem rerum, repellat facilis cum animi ut aliquam accusantium veniam laboriosam culpa in. Quos, dolor, praesentium neque eligendi natus ipsam numquam perferendis enim sint iste voluptas ipsum quidem.</p>
+            <p>Delectus a illo minus quod quibusdam officiis laudantium vero, similique vel voluptatum veritatis, non porro natus, quaerat ad soluta dolorem voluptate. Voluptatum cumque ex perferendis blanditiis odit! Ipsum veritatis earum totam dignissimos, facere impedit aliquid incidunt optio rem distinctio possimus error nobis ducimus officiis temporibus, omnis, dolor cumque? Dolore excepturi expedita consequuntur nulla aliquam perferendis tenetur cumque velit eum doloribus necessitatibus dolorem dolor, libero rem quasi error minima quos enim quis, atque exercitationem unde rerum doloremque. Quam, vero veritatis. Distinctio ut, veniam sit commodi repellendus nulla adipisci debitis, dolorum temporibus repellat reiciendis. Quas temporibus at quasi repellendus nesciunt repellat est.</p>
+            <p>Beatae illum doloribus suscipit nostrum molestias nobis commodi quis incidunt accusamus eligendi reprehenderit adipisci labore qui molestiae necessitatibus ducimus, ipsum at porro iusto! Error, eligendi? Sed officiis soluta ipsa vel, delectus, voluptate quibusdam quaerat, mollitia aspernatur assumenda ex nisi voluptas totam temporibus! Maxime iusto nesciunt vel ad et consectetur ea perspiciatis! Possimus totam explicabo, quibusdam maiores iusto animi dolor recusandae accusamus consequatur! Porro fugit neque quisquam aliquid consequatur, asperiores, quae modi, iure ducimus soluta aperiam illo voluptas dicta. Optio temporibus, voluptates harum quas illum animi. Quod ducimus pariatur saepe ut odit optio ea voluptatem, reprehenderit dolor accusamus eos qui aspernatur.</p>
+            <p>Temporibus sit asperiores deserunt alias minima magni sed nulla ipsa optio, quaerat aliquam enim error. Est repudiandae dolores, nesciunt facere sequi quia quae officia assumenda ex eaque et deleniti, animi sapiente fugiat nemo ullam quisquam. Voluptatem eligendi harum libero voluptatibus impedit maiores quo deleniti fugit a optio accusantium corporis, rerum doloribus quia animi incidunt numquam ex eaque est iusto necessitatibus, blanditiis ullam illum. Sunt sint quos, cumque cupiditate totam voluptatum explicabo tempora sed provident? Totam iure rerum nisi itaque maxime deleniti asperiores esse animi nostrum ea dignissimos quos dolor architecto iste tempora quasi beatae voluptatum, corporis tenetur vitae sequi? Quaerat.</p>
+            <p>Impedit voluptas eius assumenda quod facilis ab inventore corporis explicabo voluptatem placeat molestias fugiat ipsum saepe dolorum laborum eaque reiciendis odit, voluptatum voluptate maiores, nostrum in? Odit non, officiis ratione, dolorum nisi doloremque eum, dignissimos mollitia dolore harum soluta veniam voluptatibus ad dicta a explicabo accusamus. Nihil impedit quos molestiae, perferendis culpa, quibusdam aperiam ex odit facilis dolore quidem, sint beatae iste tempore tempora voluptates ea consequatur voluptatum ab reprehenderit? Ipsum voluptates quibusdam, sint aspernatur, quos modi laborum amet rerum perspiciatis excepturi nihil deleniti eveniet quod asperiores laudantium! Neque saepe sint sapiente qui perspiciatis sed obcaecati quidem repellat totam iste.</p>
+            <p>Nostrum, libero! Temporibus porro ducimus repellendus. Impedit odit autem enim architecto eligendi, ipsa fuga laboriosam, nulla esse corrupti placeat sed doloremque tempora quis quasi omnis modi ullam sequi! Molestiae, voluptatibus accusamus. Aliquam maxime quasi eligendi obcaecati tempore itaque exercitationem, quia molestias odio consequuntur minus provident placeat expedita repellendus saepe sequi neque nihil doloremque quis facilis in temporibus eaque, vel reprehenderit. Numquam, deleniti aspernatur sunt nobis alias veniam doloremque perspiciatis maxime voluptates eligendi, provident maiores illum quo ad ullam officia quae repellendus. Fuga, animi perferendis nisi mollitia quis earum praesentium sit natus dignissimos, eveniet libero, ullam obcaecati sed maiores ea amet?</p>
+            <p>Iure non ipsam magnam nulla vitae quibusdam quos ab necessitatibus ducimus. Animi iure accusamus iusto dolores rerum culpa doloribus esse necessitatibus, saepe consectetur expedita eveniet, est reiciendis voluptatum, minus qui! Atque totam soluta repellat facilis ea reprehenderit expedita repellendus modi nobis, ipsam corrupti est pariatur non, nostrum blanditiis debitis. Minus autem nisi veritatis repudiandae ipsam excepturi nesciunt nobis voluptates sed tempore debitis, tenetur, animi consequatur et, in inventore tempora? Deserunt rerum veritatis et hic atque quaerat dolores eligendi labore explicabo fugiat itaque voluptate reprehenderit maxime, rem placeat? Provident libero eligendi quam adipisci ullam sit, perspiciatis dolores! In illo ad quos.</p>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'Post',
+    mounted() {
+        this.$el.addEventListener('scroll', function (){
+            let scrollTop = this.$el.scrollTop
+            let clientHeight = this.$el.clientHeight
+            let scrollHeight = this.$el.scrollHeight
+            let scrollPercent = Math.round((scrollTop) / (scrollHeight - clientHeight) * 100);
+            this.$emit('progressUpdate', scrollPercent)
+        }.bind(this))
+    }
+}
+</script>
+
+<style>
+article {
+    width: 100%;
+    flex: 1 1 100%;
+    max-height: calc(100vh - 80px);
+    background: #adaaa1;
+    overflow: scroll;
+}
+
+article .content {
+    padding: 25px;
+}
+
+article .content h2 {
+    font-weight: 900;
+    font-size: 32px;
+    margin-bottom: 10px;
+}
+
+article .content h3 {
+    font-weight: 300;
+    font-size: 22px;
+    margin-bottom: 30px;
+}
+
+article .content p {
+    color: #313131;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 1.5;
+    margin-bottom: 15px;
+}
+</style>
